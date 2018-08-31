75513a68bc81a84e5e55883afdacf67fbaff880f
Jean-Marc Robin <jmr33bx@yahoo.fr>
2018-08-31 20:52:00 +0200

    Modify Main method for demo

--- src/main/java/jmr/holczer/Application.java
+++ src/main/java/jmr/holczer/Application.java
@@ -5,9 +5,17 @@ public class Application
 
 	public static void main( String[] args )
 	{
-		String data = "I love you";
-		String result = SHA256Helper.generateHash(data);
-		System.out.println(result);
+		Blockchain blockchain = new Blockchain();
+		Miner miner = new Miner();
+
+		Block block0 = new Block(0, "no transaction", "");
+		blockchain.addBlock(block0);
+
+		Block block1 = new Block(1, "transaction 1", Constants.GENESIS_PREV_HASH);
+		miner.mine(block1, blockchain);
+
+		Block block2 = new Block(2, "transaction 2", block1.getHash());
+		miner.mine(block2, blockchain);
 
 	}
 
